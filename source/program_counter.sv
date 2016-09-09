d4a020de2864ff393da22c4e52a6507c4c23ac43
Brian Rieder <brieder@purdue.edu>
2016-09-09 16:04:53 -0400

    Very basic program counter

---
+++ source/program_counter.sv
@@ -0,0 +1,23 @@
+// File name:   program_counter.sv
+// Updated:     9 September 2016
+// Author:      Brian Rieder 
+// Description: Unit to determine when to request instructions and enable data read/write
+
+// interface include
+`include "program_counter_if.vh"
+
+// memory types
+`include "cpu_types_pkg.vh"
+
+module program_counter (
+  input logic CLK, nRST,
+  program_counter_if.pc pcif
+);
+
+  always_ff @ (posedge CLK, negedge nRST) begin
+    if(pcif.pcWEN) begin
+      pcif.pc_out <= pcif.pc_next;
+    end
+  end 
+
+endmodule
