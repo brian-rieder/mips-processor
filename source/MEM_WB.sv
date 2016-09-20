ba86600ae0cf1d100547ed836228806af9d8f8e9
Brian Rieder <brieder@purdue.edu>
2016-09-20 13:53:31 -0400

    Anal spacing

--- source/MEM_WB.sv
+++ source/MEM_WB.sv
@@ -12,29 +12,29 @@ module MEM_WB (
 
 always_ff @(posedge CLK, negedge nRST) begin 
 	if (!nRST) begin  
-		memwbif.regDest_out <= '0; 
+		memwbif.regDest_out  <= '0; 
 		memwbif.dmemload_out <= '0;  
-		memwbif.regWr_out <= '0;
-        memwbif.wsel_out <= '0; 
+		memwbif.regWr_out    <= '0;
+        memwbif.wsel_out     <= '0; 
 		memwbif.memToReg_out <= '0; 
-		memwbif.halt_out <= '0;
-		memwbif.portO_out <= '0;
+		memwbif.halt_out     <= '0;
+		memwbif.portO_out    <= '0;
 		memwbif.luiValue_out <= '0; 
-		memwbif.pc4_out <= '0; 
-		memwbif.op_wb <= '0;
+		memwbif.pc4_out      <= '0; 
+		memwbif.op_wb        <= '0;
 	end 
 	else begin 
 		if (memwbif.ihit || memwbif.dhit)  begin 
-			memwbif.regDest_out <= memwbif.regDest_in; 
+			memwbif.regDest_out  <= memwbif.regDest_in; 
 			memwbif.dmemload_out <= memwbif.dmemload_in;  
-			memwbif.regWr_out <= memwbif.regWr_in;
-	        memwbif.wsel_out <= memwbif.wsel_in; 
+			memwbif.regWr_out    <= memwbif.regWr_in;
+	        memwbif.wsel_out     <= memwbif.wsel_in; 
 			memwbif.memToReg_out <= memwbif.memToReg_in; 
-			memwbif.halt_out <= memwbif.halt_in;
-			memwbif.portO_out <= memwbif.portO_in;
+			memwbif.halt_out     <= memwbif.halt_in;
+			memwbif.portO_out    <= memwbif.portO_in;
 			memwbif.luiValue_out <= memwbif.luiValue_in; 
-			memwbif.pc4_out <= memwbif.pc4_in; 
-			memwbif.op_wb <= memwbif.op_mem;
+			memwbif.pc4_out      <= memwbif.pc4_in; 
+			memwbif.op_wb        <= memwbif.op_mem;
 		end   
 	end
 	
