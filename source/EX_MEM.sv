fe573fd113dbd1a8c444a01843555c917d47471b
Brian Rieder <brieder@purdue.edu>
2016-09-19 23:15:22 -0400

    Added operations and exmemifs

--- source/EX_MEM.sv
+++ source/EX_MEM.sv
@@ -11,32 +11,33 @@ module EX_MEM (
 	
 
 always_ff @(posedge CLK, negedge nRST) begin 
-	if (nRST == 1'b0) begin  
-		regDest_out <= '0; 
-		dREN_out <= '0;
-		dWEN_out <= '0;
-		dmemstore_out <= '0;  
-		regWr_out <= '0;
-		memToReg_out <= '0; 
-		halt_out <= '0;
-		portO_out <= '0;
-		luiValue_out <= '0; 
-		pc4_out <= '0; 
+	if (!nRST) begin  
+		exmemif.regDest_out <= '0; 
+		exmemif.dREN_out <= '0;
+		exmemif.dWEN_out <= '0;
+		exmemif.dmemstore_out <= '0;  
+		exmemif.regWr_out <= '0;
+		exmemif.memToReg_out <= '0; 
+		exmemif.halt_out <= '0;
+		exmemif.portO_out <= '0;
+		exmemif.luiValue_out <= '0; 
+		exmemif.pc4_out <= '0; 
+		exmemif.op_mem <= '0;
 	end 
 	else begin 
 	
-	if(exmemif.ihit == 1'b1 || exmemif.dhit == 1'b1 )  begin 
-		exmemif.regDest_out <= exmemif.regDest_in; 
-		exmemif.dREN_out <= exmemif.dREN_in; 
-		exmemif.dWEN_out <= exmemif.dWEN_in;
-		exmemif.dmemstore_out <= exmemif.dmemstore_in;  
-		exmemif.regWr_out <= exmemif.regWr_in;
-		exmemif.memToReg_out <= exmemif.memToReg_in; 
-		exmemif.halt_out <= exmemif.halt_in;
-		exmemif.portO_out <= exmemif.portO_in;
-		exmemif.luiValue_out <= exmemif.luiValue_in;  
-		exmemif.pc4_out <= exmemif.pc4_in; 
-			 			
+		if(exmemif.ihit || exmemif.dhit )  begin 
+			exmemif.regDest_out <= exmemif.regDest_in; 
+			exmemif.dREN_out <= exmemif.dREN_in; 
+			exmemif.dWEN_out <= exmemif.dWEN_in;
+			exmemif.dmemstore_out <= exmemif.dmemstore_in;  
+			exmemif.regWr_out <= exmemif.regWr_in;
+			exmemif.memToReg_out <= exmemif.memToReg_in; 
+			exmemif.halt_out <= exmemif.halt_in;
+			exmemif.portO_out <= exmemif.portO_in;
+			exmemif.luiValue_out <= exmemif.luiValue_in;  
+			exmemif.pc4_out <= exmemif.pc4_in; 
+			exmemif.op_mem <= exmemif.op_ex;
 		end   
 	end
 	
